6dd90b790b0bec4fdc20943d9c308182dab5171e
Maddie <115709272+Maddily@users.noreply.github.com>
2024-02-23 12:51:57 +0200

    Undo change

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,5 @@
 # Install flask from pip3
-
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
-}
-
-package { 'python3-pip':
-  ensure => installed,
 }
