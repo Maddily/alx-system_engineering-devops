6f8828b76826c50c9af567ce7a1182a7a485e82b
Maddie <115709272+Maddily@users.noreply.github.com>
2024-02-23 14:01:45 +0200

    Install packages

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,19 @@
+# Ensure Python 3.8.10 is installed
+
+package { 'python3.8':
+  ensure => '3.8.10',
+}
+
 # Install flask from pip3
+
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+# Ensure Werkzeug 2.1.1 is installed
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
