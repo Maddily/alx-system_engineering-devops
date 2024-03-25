de2a9bf7600d6a54b7475c43253961adc7078481
Maddie <115709272+Maddily@users.noreply.github.com>
2024-03-25 15:26:58 +0200

    Use an API to retrieve data

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module uses an API to export information about an employee's
+todo list in csv format.
+"""
+
+if __name__ == '__main__':
+
+    import csv
+    import requests
+    import sys
+
+    employee_id = int(sys.argv[1])
+
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
+
+    employee_username = users.json()[employee_id - 1]['username']
+
+    with open(f'{employee_id}.csv', 'w') as file:
+        writer = csv.writer(file)
+        for todo in todos.json():
+            if todo['userId'] == employee_id:
+                task_status = todo['completed']
+                task = todo['title']
+                file.write(f'"{employee_id}","{employee_username}",'
+                           f'"{task_status}","{task}"\n')
