13773c0db7cd0162d892d35c9f0a4a8c23637d05
Maddie <115709272+Maddily@users.noreply.github.com>
2024-04-11 06:49:03 +0200

    Update command

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# Manage the content of wp-settings.php file,
-# replacing .phpp with .php
-file { '/var/www/html/wp-settings.php':
-    ensure  => present,
-    content => file('/var/www/html/wp-settings.php').content.gsub('.phpp', '.php'),
+# Manage the content of wp-settings.php file, replacing .phpp with .php
+
+exec { 'Correct php':
+  command  => 'sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
